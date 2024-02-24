6d251f5273910d238d590f01933e1b79bd6f97ac
Saajid S. <saajids@nycstudents.net>
2024-02-24 14:04:54 -0500

    Initial commit

---
+++ src/main/java/net/yummuy/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.yummuy.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
